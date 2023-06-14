ec102d409291ce01960f7111b324b9bbd796bff0
stan <mail2emzy@gmail.com>
2023-06-14 11:05:31 +0100

    More Changes

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router basename="/peacemass">
+    <Router basename="/peacemass/">
       <div className="App">
         <Header />
         <Routes>
